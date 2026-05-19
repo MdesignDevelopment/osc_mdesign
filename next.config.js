9e790c98560d01b20461cf7be9465883a565bdd3
Musa Haruna <musa.haruna@mdesignsolutions.be>
2026-05-19 13:28:57 +0100

    Revert "Configure for Vercel + Neon deployment"
    This reverts commit 7dd310153a12c9594d4ee983728637d8286262e5.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     unoptimized: true,
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000'],
+    },
   },
 }
 
